4758f30b3801432623e6f5de7269930f97f926ae
nick leslie <nleslie@live.com>
2025-01-30 10:16:15 -0600

    Added files

---
+++ http-server/file_system_store.go
@@ -0,0 +1,16 @@
+package httpserver
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type FileSystemPlayerStore struct {
+	database io.Reader
+}
+
+func (f *FileSystemPlayerStore) GetLeague() []Player {
+	var league []Player
+	json.NewDecoder(f.database).Decode(&league)
+	return league
+}
---
+++ http-server/testHelpers.go
@@ -0,0 +1,13 @@
+package httpserver
+
+import (
+	"reflect"
+	"testing"
+)
+
+func assertLeague(t testing.TB, got, want []Player) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
